5a0473ffb72b5038275014db26b2fcce36edc1af
Andres Gonzalez Perez <andresmgp97@gmail.com>
2025-12-15 18:24:55 +0100

    About Page Updates

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,16 +1,25 @@
+import valentinImg from "../assets/valentin.jpg";
+import andresImg from "../assets/andres.JPG";
+
 const AboutPage = () => {
   return (
-    <div>
+    <div className="about-page">
       <h1>Our Team</h1>
-      <p>Our team is made up of the best chefs in the world, specializing in the preparation of street food recipes.</p>
+      <p>
+        Our team is made up of the best chefs in the world, specializing in the
+        preparation of street food recipes.
+      </p>
 
       <h2>Team</h2>
       <ul>
-        <li>
-          Valentín
+        <li className="team-member">
+          <span className="team-name">Marian Valentín</span>
+          <img src={valentinImg} alt="Marian Valentín" className="team-img" />
         </li>
-        <li>
-          Andrés González
+
+        <li className="team-member">
+          <span className="team-name">Andrés González</span>
+          <img src={andresImg} alt="Andrés González" className="team-img" />
         </li>
       </ul>
     </div>
@@ -18,4 +27,3 @@ const AboutPage = () => {
 };
 
 export default AboutPage;
-
